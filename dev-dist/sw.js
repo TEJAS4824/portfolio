f3b010e20d39276d77b8511c8cad937a49ad23d1
tejas2435 <tejasmistry2017@gmail.com>
2026-04-18 13:20:14 +0530

    feat: add service worker for asset precaching and navigation routing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.h0qfghk1fsg"
+    "revision": "0.gv1j2pidee"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
